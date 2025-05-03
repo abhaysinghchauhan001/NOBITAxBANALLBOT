4c7ae551260e2d605f818ba362987d90aed4d17b
abhaysinghchauhan001 <chauhanabhay206243@gmail.com>
2025-05-03 18:15:25 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+class Config:
+    API_ID=28269355
+    API_HASH="805b8c6577a8c30db439d901af544cac"
+    TOKEN="7472707188:AAH8u4HvaldKVbQON1eLZ76hznarrrp8T20"
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "5536473064").split(" "))
+    START_IMG="https://files.catbox.moe/7qyorb.jpg"
+    BOT_ID=7156025226
+    BOT_USERNAME="NOBITA_BANALL_BOT"
+    BOT_NAME="『 𝐍𝐎𝐁𝐈𝐓𝐀 ✘ 𝐁𝐀𝐍𝐀𝐋𝐋 』"
